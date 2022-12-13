82664fb757a38d0f7e3991daf69c622db6a6eaef
braedynl <braedynlettinga@gmail.com>
2022-12-12 22:34:43 -0500

    Merge utilities stub with module

--- matrices/utilities.py
+++ matrices/utilities.py
@@ -1,6 +1,8 @@
 import itertools
+from collections.abc import Iterator
+from typing import Any, Protocol, TypeVar
 
-from .protocols import MatrixLike
+from .protocols import MatrixLike, ShapeLike
 
 __all__ = [
     "only",
@@ -12,8 +14,19 @@ __all__ = [
     "conjugate",
 ]
 
+T = TypeVar("T")
+T_co = TypeVar("T_co", covariant=True)
 
-def only(x):
+
+class SupportsConjugate(Protocol[T_co]):
+    def conjugate(self) -> T_co: ...
+
+class SupportsLenAndGetItem(Protocol[T_co]):
+    def __len__(self) -> int: ...
+    def __getitem__(self, key: int) -> T_co: ...
+
+
+def only(x: SupportsLenAndGetItem[T]) -> T:
     """Return the only contained object of a length 1 collection
 
     Raises `ValueError` if the collection does not have length 1.
@@ -23,7 +36,7 @@ def only(x):
     return x[0]
 
 
-def likewise(x, shape):
+def likewise(x: MatrixLike[T] | T, shape: ShapeLike) -> Iterator[T]:
     """Return an iterator of `x` as if it were a matrix of `shape`
 
     Raises `ValueError` under the following conditions:
@@ -47,26 +60,26 @@ def likewise(x, shape):
     return it
 
 
-def logical_and(a, b, /):
+def logical_and(a: Any, b: Any, /) -> bool:
     """Return the logical AND of `a` and `b`"""
     return not not (a and b)
 
 
-def logical_or(a, b, /):
+def logical_or(a: Any, b: Any, /) -> bool:
     """Return the logical OR of `a` and `b`"""
     return not not (a or b)
 
 
-def logical_xor(a, b, /):
+def logical_xor(a: Any, b: Any, /) -> bool:
     """Return the logical XOR of `a` and `b`"""
     return (not not a) is not (not not b)
 
 
-def logical_not(a, /):
+def logical_not(a: Any, /) -> bool:
     """Return the logical NOT of `a`"""
     return not a
 
 
-def conjugate(x, /):
+def conjugate(x: SupportsConjugate[T], /) -> T:
     """Return the object's conjugate"""
     return x.conjugate()
