ce05b0638b6c4799cbc33147fe70b4d450185bac
Braedyn Lettinga <braedynlettinga@gmail.com>
2022-12-25 15:35:21 -0500

    Limit common utilities to `checked_map()`

--- matrices/utilities/common.py
+++ matrices/utilities/common.py
@@ -1,7 +1,4 @@
-import functools
-import operator
-
-__all__ = ["checked_map", "compare", "multiply"]
+__all__ = ["checked_map"]
 
 
 def checked_map(func, a, *bx):
@@ -10,36 +7,3 @@ def checked_map(func, a, *bx):
         if u != (v := b.shape):
             raise ValueError(f"matrix of shape {u} is incompatible with operand shape {v}")
     yield from map(func, a, *bx)
-
-
-def compare(a, b):
-    u, v = (a.shape, b.shape)
-    if u != v:
-        raise ValueError(f"matrix of shape {u} is incompatible with operand shape {v}")
-    for x, y in zip(a, b):
-        if x < y:
-            return -1
-        if x > y:
-            return 1
-    return 0
-
-
-def multiply(a, b):
-    u, v = (a.shape, b.shape)
-    m, n = u
-    p, q = v
-    if n != p:
-        raise ValueError(f"matrix of shape {u} is incompatible with operand shape {v}")
-    if not n:
-        raise ValueError  # TODO: error message
-    ix = range(m)
-    jx = range(q)
-    kx = range(n)
-    yield from (
-        functools.reduce(
-            operator.add,
-            map(lambda k: a[i * n + k] * b[k * q + j], kx),
-        )
-        for i in ix
-        for j in jx
-    )
--- matrices/utilities/common.pyi
+++ matrices/utilities/common.pyi
@@ -1,11 +1,9 @@
 from collections.abc import Callable, Iterator
-from typing import Literal, TypeVar, overload
+from typing import TypeVar, overload
 
 from ..abstract import MatrixLike
-from ..typeshed import (SupportsClosedAdd, SupportsDotProduct,
-                        SupportsRDotProduct)
 
-__all__ = ["checked_map", "compare", "multiply"]
+__all__ = ["checked_map"]
 
 T = TypeVar("T")
 
@@ -13,12 +11,12 @@ T1 = TypeVar("T1")
 T2 = TypeVar("T2")
 T3 = TypeVar("T3")
 T4 = TypeVar("T4")
+T5 = TypeVar("T5")
+Tx = TypeVar("Tx")
 
 M = TypeVar("M", bound=int)
 N = TypeVar("N", bound=int)
 P = TypeVar("P", bound=int)
-
-SupportsClosedAddT = TypeVar("SupportsClosedAddT", bound=SupportsClosedAdd)
 
 @overload
 def checked_map(func: Callable[[T1], T], a: MatrixLike[T1, M, N]) -> Iterator[T]: ...
@@ -28,8 +26,7 @@ def checked_map(func: Callable[[T1, T2], T], a: MatrixLike[T1, M, N], b: MatrixL
 def checked_map(func: Callable[[T1, T2, T3], T], a: MatrixLike[T1, M, N], b: MatrixLike[T2, M, N], c: MatrixLike[T3, M, N]) -> Iterator[T]: ...
 @overload
 def checked_map(func: Callable[[T1, T2, T3, T4], T], a: MatrixLike[T1, M, N], b: MatrixLike[T2, M, N], c: MatrixLike[T3, M, N], d: MatrixLike[T4, M, N]) -> Iterator[T]: ...
-def compare(a: MatrixLike[T, M, N], b: MatrixLike[T, M, N]) -> Literal[-1, 0, 1]: ...
 @overload
-def multiply(a: MatrixLike[SupportsDotProduct[T, SupportsClosedAddT], M, N], b: MatrixLike[T, N, P]) -> Iterator[SupportsClosedAddT]: ...
+def checked_map(func: Callable[[T1, T2, T3, T4, T5], T], a: MatrixLike[T1, M, N], b: MatrixLike[T2, M, N], c: MatrixLike[T3, M, N], d: MatrixLike[T4, M, N], e: MatrixLike[T5, M, N]) -> Iterator[T]: ...
 @overload
-def multiply(a: MatrixLike[T, M, N], b: MatrixLike[SupportsRDotProduct[T, SupportsClosedAddT], N, P]) -> Iterator[SupportsClosedAddT]: ...
+def checked_map(func: Callable[..., T], a: MatrixLike[T1, M, N], *bx: MatrixLike[Tx, M, N]) -> Iterator[T]: ...
