1542f835a8d8ffc62f89e88450bde2c3975555d0
braedynl <braedynlettinga@gmail.com>
2022-12-26 16:38:26 -0500

    Minor changes to `checked_map()`

--- matrices/utilities/common.py
+++ matrices/utilities/common.py
@@ -2,8 +2,17 @@ __all__ = ["checked_map"]
 
 
 def checked_map(func, a, *bx):
+    """Return an iterator that computes a function from the arguments of
+    equally-shaped matrices
+
+    Raises `ValueError` if there exists a matrix whose shape differs from the
+    others.
+    """
+    if not bx:
+        yield from map(func, a)
+        return
     u = a.shape
     for b in bx:
         if u != (v := b.shape):
-            raise ValueError(f"matrix of shape {u} is incompatible with operand shape {v}")
+            raise ValueError(f"incompatible shapes {u}, {v}")
     yield from map(func, a, *bx)
