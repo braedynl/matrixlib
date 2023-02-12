4e1cddef9cd93028cce1556e3b64e96ea2d96500
braedynl <braedynlettinga@gmail.com>
2023-02-12 02:51:34 -0500

    Remove unnecessary `__all__` variables

--- matrices/__init__.py
+++ matrices/__init__.py
@@ -1,12 +1,3 @@
 from .abc import *
 from .builtins import *
 from .rule import *
-
-__all__ = [
-    "MatrixLike",
-    "FrozenMatrix",
-    "Matrix",
-    "Rule",
-    "ROW",
-    "COL",
-]
--- matrices/views/__init__.py
+++ matrices/views/__init__.py
@@ -1,12 +1,2 @@
 from .abc import *
 from .builtins import *
-
-__all__ = [
-    "MatrixViewLike",
-    "MatrixView",
-    "MatrixTransform",
-    "MatrixTranspose",
-    "MatrixRowFlip",
-    "MatrixColFlip",
-    "MatrixReverse",
-]
