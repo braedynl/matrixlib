d75d07abc6f4753ae288cbf842f5f2ddf7975290
braedynl <braedynlettinga@gmail.com>
2023-02-13 14:41:05 -0500

    Declare logical operators in module

--- matrices/utilities/operator.py
+++ matrices/utilities/operator.py
@@ -3,7 +3,17 @@ from operator import and_ as bitwise_and
 from operator import (eq, floordiv, ge, gt, invert, le, lshift, lt, mod, mul,
                       ne, neg)
 from operator import or_ as bitwise_or
-from operator import pos, pow, rshift, sub, truediv
+from operator import pos, rshift, sub, truediv
 from operator import xor as bitwise_xor
 
-from logical_operator import logical_and, logical_not, logical_or
+
+def logical_and(a, b, /) -> bool:
+    return (not not a) and (not not b)
+
+
+def logical_or(a, b, /) -> bool:
+    return (not not a) or (not not b)
+
+
+def logical_not(a, /) -> bool:
+    return not a
