7f73709491e866f21437921765a3dcf997ce41c0
braedynl <braedynlettinga@gmail.com>
2023-02-15 04:08:30 -0500

    New naming convention

--- matrices/utilities/matrix_operator.py
+++ matrices/utilities/matrix_operator.py
@@ -1,93 +1,107 @@
-from builtins import abs as _abs
-from builtins import divmod as _divmod
-from operator import add as _add
-from operator import and_ as _bitwise_and
-from operator import eq as _equal
-from operator import floordiv as _floordiv
-from operator import ge as _greater_equal
-from operator import gt as _greater
-from operator import invert as _invert
-from operator import le as _lesser_equal
-from operator import lshift as _lshift
-from operator import lt as _lesser
-from operator import mod as _mod
-from operator import mul as _mul
-from operator import ne as _not_equal
-from operator import neg as _neg
-from operator import or_ as _bitwise_or
-from operator import pos as _pos
-from operator import rshift as _rshift
-from operator import sub as _sub
-from operator import truediv as _truediv
-from operator import xor as _bitwise_xor
+import operator
 
 from .vectorize import vectorize
 
 __all__ = [
-    "abs",
-    "divmod",
+    "cmp",
+    "lt",
+    "le",
+    "eq",
+    "ne",
+    "gt",
+    "ge",
     "add",
-    "bitwise_and",
-    "equal",
-    "floordiv",
-    "greater_equal",
-    "greater",
-    "invert",
-    "lesser_equal",
-    "lshift",
-    "lesser",
-    "mod",
-    "mul",
-    "not_equal",
-    "neg",
-    "bitwise_or",
-    "pos",
-    "rshift",
     "sub",
-    "truediv",
-    "bitwise_xor",
-    "logical_and",
-    "logical_or",
-    "logical_not",
-    "conjugate",
+    "mul",
+    "div",
+    "quo",
+    "rem",
+    "quo_rem",
+    "shl",
+    "shr",
+    "neg",
+    "pos",
+    "abs",
+    "bit_and",
+    "bit_xor",
+    "bit_or",
+    "bit_inv",
+    "log_and",
+    "log_xor",
+    "log_or",
+    "mat_mul",
+    "conj",
 ]
 
-abs = vectorize()(_abs)
-divmod = vectorize()(_divmod)
-add = vectorize()(_add)
-bitwise_and = vectorize()(_bitwise_and)
-equal = vectorize()(_equal)
-floordiv = vectorize()(_floordiv)
-greater_equal = vectorize()(_greater_equal)
-greater = vectorize()(_greater)
-invert = vectorize()(_invert)
-lesser_equal = vectorize()(_lesser_equal)
-lshift = vectorize()(_lshift)
-lesser = vectorize()(_lesser)
-mod = vectorize()(_mod)
-mul = vectorize()(_mul)
-not_equal = vectorize()(_not_equal)
-neg = vectorize()(_neg)
-bitwise_or = vectorize()(_bitwise_or)
-pos = vectorize()(_pos)
-rshift = vectorize()(_rshift)
-sub = vectorize()(_sub)
-truediv = vectorize()(_truediv)
-bitwise_xor = vectorize()(_bitwise_xor)
 
+def cmp(a, b, /):
+    if a is b:
+        return 0
+    for x, y in zip(a, b):
+        if x == y:
+            continue
+        if x < y:
+            return -1
+        if x > y:
+            return 1
+        raise RuntimeError
+    u = a.shape
+    v = b.shape
+    if u is v:
+        return 0
+    for m, n in zip(u, v):
+        if m == n:
+            continue
+        if m < n:
+            return -1
+        if m > n:
+            return 1
+    return 0
+
+lt = vectorize()(operator.__lt__)
+le = vectorize()(operator.__le__)
+eq = vectorize()(operator.__eq__)
+ne = vectorize()(operator.__ne__)
+gt = vectorize()(operator.__gt__)
+ge = vectorize()(operator.__ge__)
+
+add     = vectorize()(operator.__add__)
+sub     = vectorize()(operator.__sub__)
+mul     = vectorize()(operator.__mul__)
+div     = vectorize()(operator.__truediv__)
+quo     = vectorize()(operator.__floordiv__)
+rem     = vectorize()(operator.__mod__)
+quo_rem = vectorize()(divmod)
+shl     = vectorize()(operator.__lshift__)
+shr     = vectorize()(operator.__rshift__)
+neg     = vectorize()(operator.__neg__)
+pos     = vectorize()(operator.__pos__)
+abs     = vectorize()(abs)
+
+bit_and = vectorize()(operator.__and__)
+bit_xor = vectorize()(operator.__xor__)
+bit_or  = vectorize()(operator.__or__)
+bit_inv = vectorize()(operator.__invert__)
 
 @vectorize()
-def logical_and(a, b, /):
+def log_and(a, b, /):
     return not not (a and b)
 
 @vectorize()
-def logical_or(a, b, /):
+def log_xor(a, b, /):
+    return (not a) is not (not b)
+
+@vectorize()
+def log_or(a, b, /):
     return not not (a or b)
 
 @vectorize()
-def logical_not(a, /):
+def log_inv(a, /):
     return not a
 
+def mat_mul(a, b, /):  # TODO: needs proper stub as well
+    pass
+
 @vectorize()
-def conjugate(a, /):
+def conj(a, /):
     return a.conjugate()
