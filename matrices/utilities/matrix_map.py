5a21aaf16aa33a5e16ae49014cd0e223a628404d
braedynl <braedynlettinga@gmail.com>
2023-02-21 15:03:36 -0500

    Use S instead of T

--- matrices/utilities/matrix_map.py
+++ matrices/utilities/matrix_map.py
@@ -11,13 +11,13 @@ T3 = TypeVar("T3")
 T4 = TypeVar("T4")
 T5 = TypeVar("T5")
 
-T_co = TypeVar("T_co", covariant=True)
+S_co = TypeVar("S_co", covariant=True)
 
 M_co = TypeVar("M_co", covariant=True, bound=int)
 N_co = TypeVar("N_co", covariant=True, bound=int)
 
 
-class MatrixMap(ShapedCollection[T_co, M_co, N_co]):
+class MatrixMap(ShapedCollection[S_co, M_co, N_co]):
     """A ``ShapedCollection`` wrapper for mapping processes on ``MatrixLike``
     objects
 
@@ -31,14 +31,14 @@ class MatrixMap(ShapedCollection[T_co, M_co, N_co]):
     @overload
     def __init__(
         self,
-        f: Callable[[T1], T_co],
+        f: Callable[[T1], S_co],
         a: ShapedIterable[T1, M_co, N_co],
         /,
     ) -> None: ...
     @overload
     def __init__(
         self,
-        f: Callable[[T1, T2], T_co],
+        f: Callable[[T1, T2], S_co],
         a: ShapedIterable[T1, M_co, N_co],
         b: ShapedIterable[T2, M_co, N_co],
         /,
@@ -46,7 +46,7 @@ class MatrixMap(ShapedCollection[T_co, M_co, N_co]):
     @overload
     def __init__(
         self,
-        f: Callable[[T1, T2, T3], T_co],
+        f: Callable[[T1, T2, T3], S_co],
         a: ShapedIterable[T1, M_co, N_co],
         b: ShapedIterable[T2, M_co, N_co],
         c: ShapedIterable[T3, M_co, N_co],
@@ -55,7 +55,7 @@ class MatrixMap(ShapedCollection[T_co, M_co, N_co]):
     @overload
     def __init__(
         self,
-        f: Callable[[T1, T2, T3, T4], T_co],
+        f: Callable[[T1, T2, T3, T4], S_co],
         a: ShapedIterable[T1, M_co, N_co],
         b: ShapedIterable[T2, M_co, N_co],
         c: ShapedIterable[T3, M_co, N_co],
@@ -65,7 +65,7 @@ class MatrixMap(ShapedCollection[T_co, M_co, N_co]):
     @overload
     def __init__(
         self,
-        f: Callable[[T1, T2, T3, T4, T5], T_co],
+        f: Callable[[T1, T2, T3, T4, T5], S_co],
         a: ShapedIterable[T1, M_co, N_co],
         b: ShapedIterable[T2, M_co, N_co],
         c: ShapedIterable[T3, M_co, N_co],
@@ -76,7 +76,7 @@ class MatrixMap(ShapedCollection[T_co, M_co, N_co]):
     @overload
     def __init__(
         self,
-        f: Callable[..., T_co],
+        f: Callable[..., S_co],
         a: ShapedIterable[Any, M_co, N_co],
         /,
         *bx: ShapedIterable[Any, M_co, N_co],
@@ -92,7 +92,7 @@ class MatrixMap(ShapedCollection[T_co, M_co, N_co]):
         self._args  = (a, *bx)
         self._shape = u
 
-    def __iter__(self) -> Iterator[T_co]:
+    def __iter__(self) -> Iterator[S_co]:
         yield from map(self._func, *self._args)
 
     @property
