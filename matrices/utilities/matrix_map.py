52e63764834ca66fe9481083750e534ad97b6322
braedynl <braedynlettinga@gmail.com>
2023-02-25 19:35:22 -0500

    Use future annotations import everywhere

--- matrices/utilities/matrix_map.py
+++ matrices/utilities/matrix_map.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from collections.abc import Callable, Iterator
 from typing import Any, TypeVar, overload
 
--- matrices/utilities/matrix_product.py
+++ matrices/utilities/matrix_product.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import itertools
 import operator
 from collections.abc import Iterable, Iterator
