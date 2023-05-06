71933010ffd604253c29e329d90c6a321d1da3be
braedynl <braedynlettinga@gmail.com>
2023-05-06 14:16:51 -0400

    Update sheers

--- matrixlib/sieves/sheers.py
+++ matrixlib/sieves/sheers.py
@@ -1,10 +1,10 @@
 from typing import Any, Generic, Literal, TypeVar
 
-from .abc import MatrixSieve, Sieve
+from .abc import BaseAccessor, BaseMatrixAccessor
 
 __all__ = [
-    "RowSheer",
-    "ColSheer",
+    "RowSheerAccessor",
+    "ColSheerAccessor",
 ]
 
 T_co = TypeVar("T_co", covariant=True)
@@ -12,49 +12,55 @@ M_co = TypeVar("M_co", covariant=True, bound=int)
 N_co = TypeVar("N_co", covariant=True, bound=int)
 
 
-class RowSheer(MatrixSieve[Literal[1], N_co, T_co], Generic[N_co, T_co]):
+class RowSheerAccessor(BaseMatrixAccessor[Literal[1], N_co, T_co], Generic[N_co, T_co]):
 
     __slots__ = ("target", "row_index")
 
-    target: Sieve[Any, N_co, T_co]
+    target: BaseAccessor[Any, N_co, T_co]
     row_index: int
     nrows: Literal[1] = 1
 
-    def __init__(self, target: Sieve[Any, N_co, T_co], *, row_index: int) -> None:
+    def __init__(self, target: BaseAccessor[Any, N_co, T_co], *, row_index: int) -> None:
         self.target = target
         self.row_index = row_index
 
     def __repr__(self) -> str:
-        return f"RowSheer(target={self.target!r}, row_index={self.row_index!r})"
+        return f"RowSheerAccessor(target={self.target!r}, row_index={self.row_index!r})"
+
+    def __hash__(self) -> int:
+        return hash((self.target, self.row_index))
 
     @property
     def ncols(self) -> N_co:
         return self.target.ncols
 
-    def matrix_collect(self, row_index: int, col_index: int) -> T_co:
+    def matrix_access(self, row_index: int, col_index: int) -> T_co:
         row_index = self.row_index + row_index
-        return self.target.matrix_collect(row_index, col_index)
+        return self.target.matrix_access(row_index, col_index)
 
 
-class ColSheer(MatrixSieve[M_co, Literal[1], T_co], Generic[M_co, T_co]):
+class ColSheerAccessor(BaseMatrixAccessor[M_co, Literal[1], T_co], Generic[M_co, T_co]):
 
     __slots__ = ("target", "col_index")
 
-    target: Sieve[M_co, Any, T_co]
+    target: BaseAccessor[M_co, Any, T_co]
     col_index: int
     ncols: Literal[1] = 1
 
-    def __init__(self, target: Sieve[M_co, Any, T_co], *, col_index: int) -> None:
+    def __init__(self, target: BaseAccessor[M_co, Any, T_co], *, col_index: int) -> None:
         self.target = target
         self.col_index = col_index
 
     def __repr__(self) -> str:
-        return f"ColSheer(target={self.target!r}, col_index={self.col_index!r})"
+        return f"ColSheerAccessor(target={self.target!r}, col_index={self.col_index!r})"
+
+    def __hash__(self) -> int:
+        return hash((self.target, self.col_index))
 
     @property
     def nrows(self) -> M_co:
         return self.target.nrows
 
-    def matrix_collect(self, row_index: int, col_index: int) -> T_co:
+    def matrix_access(self, row_index: int, col_index: int) -> T_co:
         col_index = self.col_index + col_index
-        return self.target.matrix_collect(row_index, col_index)
+        return self.target.matrix_access(row_index, col_index)
