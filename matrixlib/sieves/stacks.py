8f1e59be92878e44a95075084f1ff686a3ad3a6a
braedynl <braedynlettinga@gmail.com>
2023-05-06 14:22:10 -0400

    Update stacks

--- matrixlib/sieves/stacks.py
+++ matrixlib/sieves/stacks.py
@@ -1,11 +1,11 @@
-import itertools
+from collections.abc import Iterator
 from typing import TypeVar
 
-from .abc import MatrixSieve, Sieve
+from .abc import BaseAccessor, BaseMatrixAccessor
 
 __all__ = [
-    "RowStack",
-    "ColStack",
+    "RowStackAccessor",
+    "ColStackAccessor",
 ]
 
 T_co = TypeVar("T_co", covariant=True)
@@ -13,19 +13,22 @@ M_co = TypeVar("M_co", covariant=True, bound=int)
 N_co = TypeVar("N_co", covariant=True, bound=int)
 
 
-class RowStack(MatrixSieve[int, N_co, T_co]):
+class RowStackAccessor(BaseMatrixAccessor[int, N_co, T_co]):
 
     __slots__ = ("target_head", "target_body")
 
-    target_head: Sieve[int, N_co, T_co]
-    target_body: tuple[Sieve[int, N_co, T_co], ...]
+    target_head: BaseAccessor[int, N_co, T_co]
+    target_body: tuple[BaseAccessor[int, N_co, T_co], ...]
 
-    def __init__(self, target_head: Sieve[int, N_co, T_co], target_body: tuple[Sieve[int, N_co, T_co], ...]) -> None:
+    def __init__(self, target_head: BaseAccessor[int, N_co, T_co], target_body: tuple[BaseAccessor[int, N_co, T_co], ...]) -> None:
         self.target_head = target_head
         self.target_body = target_body
 
     def __repr__(self) -> str:
-        return f"RowStack(target_head={self.target_head!r}, target_body={self.target_body!r})"
+        return f"RowStackAccessor(target_head={self.target_head!r}, target_body={self.target_body!r})"
+
+    def __hash__(self) -> int:
+        return hash((self.target_head, self.target_body))
 
     @property
     def nrows(self) -> int:
@@ -38,29 +41,36 @@ class RowStack(MatrixSieve[int, N_co, T_co]):
     def ncols(self) -> N_co:
         return self.target_head.ncols
 
-    def matrix_collect(self, row_index: int, col_index: int) -> T_co:
-        for target in itertools.chain((self.target_head,), self.target_body):
+    def matrix_access(self, row_index: int, col_index: int) -> T_co:
+        for target in self.targets():
             nrows = target.nrows
             if (row_index >= nrows):
                 row_index -= nrows
             else:
                 break
-        return target.matrix_collect(row_index, col_index)
+        return target.matrix_access(row_index, col_index)
+
+    def targets(self) -> Iterator[BaseAccessor[int, N_co, T_co]]:
+        yield self.target_head
+        yield from self.target_body
 
 
-class ColStack(MatrixSieve[M_co, int, T_co]):
+class ColStackAccessor(BaseMatrixAccessor[M_co, int, T_co]):
 
     __slots__ = ("target_head", "target_body")
 
-    target_head: Sieve[M_co, int, T_co]
-    target_body: tuple[Sieve[M_co, int, T_co], ...]
+    target_head: BaseAccessor[M_co, int, T_co]
+    target_body: tuple[BaseAccessor[M_co, int, T_co], ...]
 
-    def __init__(self, target_head: Sieve[M_co, int, T_co], target_body: tuple[Sieve[M_co, int, T_co], ...]) -> None:
+    def __init__(self, target_head: BaseAccessor[M_co, int, T_co], target_body: tuple[BaseAccessor[M_co, int, T_co], ...]) -> None:
         self.target_head = target_head
         self.target_body = target_body
 
     def __repr__(self) -> str:
-        return f"ColStack(target_head={self.target_head!r}, target_body={self.target_body!r})"
+        return f"ColStackAccessor(target_head={self.target_head!r}, target_body={self.target_body!r})"
+
+    def __hash__(self) -> int:
+        return hash((self.target_head, self.target_body))
 
     @property
     def nrows(self) -> M_co:
@@ -73,11 +83,15 @@ class ColStack(MatrixSieve[M_co, int, T_co]):
             + sum(map(lambda target: target.ncols, self.target_body))
         )
 
-    def matrix_collect(self, row_index: int, col_index: int) -> T_co:
-        for target in itertools.chain((self.target_head,), self.target_body):
+    def matrix_access(self, row_index: int, col_index: int) -> T_co:
+        for target in self.targets():
             ncols = target.ncols
             if (col_index >= ncols):
                 col_index -= ncols
             else:
                 break
-        return target.matrix_collect(row_index, col_index)
+        return target.matrix_access(row_index, col_index)
+
+    def targets(self) -> Iterator[BaseAccessor[M_co, int, T_co]]:
+        yield self.target_head
+        yield from self.target_body
